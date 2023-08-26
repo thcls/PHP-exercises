9b9e2b9e1cc1918a12e5ecf5eab3eb7e7b3326d1
Thiago Carlos <thiagocarlossilvapereira@gmail.com>
2023-08-26 00:14:57 -0300

    feat: exercício 000

---
+++ ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando o PHP</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        echo "Hello world!"
+        ?>
+    </h1>
+    <p>Teste 1,2,3.</p>
+</body>
+
+</html>
